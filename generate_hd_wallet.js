1da855f3cfc4deb1b3e96bfdb2c34e3547aa5e08
ch4ot1c <me@jonl.io>
2018-05-10 20:50:04 -0500

    Cleanup, beautify+clarify generate_hd_wallet output

--- generate_hd_wallet.js
+++ generate_hd_wallet.js
@@ -7,10 +7,13 @@ const Mnemonic = require('bitcore-mnemonic');
 const externalAddrPath = "m/44'/183'/0'"; // BIP-0044 + SLIP-0044
 
 
-console.log('Generating your Master Private Key - this will only be shown ONCE!');
-//TODO press 'Y' button to generate + display
+console.log('\nGenerating your Master Private Key - this will only be shown ONCE!');
+console.log('\nThere are two representations - xprv or Mnemonic Seed Words. You will be shown both.');
+console.log('\nFor reference the "HD Derivation Path" used is m/44\'/183\'0\'/0/0 - you should store this information with your keys.');
+//TODO console.log("Press 'Y' to generate and display your Master Private Key"); 
+//TODO bright colors
 
-//TODO Prompt for an optional password
+//TODO Prompt for an optional password (should we even?)
 //const pwd = 'OPTIONAL PASSWORD';
 
 var seed = new Mnemonic(Mnemonic.Words.ENGLISH);
@@ -19,41 +22,29 @@ var xprv = seed.toHDPrivateKey();
 var hdPublicKey = xprv.deriveChild(externalAddrPath).hdPublicKey;
 
 
-// Setup Report
+// --- Setup Report ---
 
-console.log('Master Private Key (SHH! - never need to input or display, even on your own server) (two variants): ');
-console.log('');
+console.log('\n---');
+console.log('\nMaster Private Key: ');
+console.log('\nSHH! - you never need to input or display these, even on your own server.');
 
-console.log('xprv (more robust): ');
+console.log('\nxprv: ');
 console.log(xprv.toString());
-console.log('');
-console.log('Seed Words (easier to remember) ("HD Derivation Path" is m/44\'/183\'/0\'): ');
-console.log(seed);
-console.log('');
+console.log('\nMnemonic Seed Words (easier to write down) ("HD Derivation Path" is m/44\'/183\'/0\'): ');
+console.log(seed.toString());
+
+console.log('\nMaster Public Key: ');
 
 var xpubkey = hdPublicKey.xpubkey.toString();
 //TODO store xpubkey in mongo Merchant
-console.log('xpub (this will be stored on your server, in MongoDB, to safely generate all addresses): ');
+console.log('\nDerived xpub (this is now stored on your server, in MongoDB, to safely generate all addresses): ');
 console.log(xpubkey);
-console.log('');
+
+console.log('\n---');
 
 // EXAMPLE - store-demo - select only the first address
 const addressIndex = "0";
 
 var address = hdPublicKey.deriveChild("m/0/" + addressIndex).publicKey.toAddress();
-console.log('First address: ', address);
-
-
-/*
-//TODO Remove
-//Full Access - Derive Private Keys - (You shouldn't ever need to use this for invoices/addresses) 
-var firstKey = xprv.deriveChild(externalAddrPath + "/0/addressIndex").privateKey;
-console.log('First PRIVATE KEY (compressed)');
-console.log(firstKey);
-console.log(firstKey.toWIF());
-
-var firstAddress = firstKey.toAddress();
-console.log('First address');
-console.log(firstAddress);
-*/
-
+console.log('\nFirst address: ', address);
+console.log('');
