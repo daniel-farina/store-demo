803ec884c9c710d6cbb7cd6983af49d4354b51e3
Jon Layton <me@jonl.io>
2018-05-12 00:38:11 +0000

    Support mongoURL option, use node.log, import bluebird

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 var EventEmitter = require('events').EventEmitter;
 const fs = require('fs');
-const bitcore = require('bitcore-lib');
+const bitcore = require('bitcore-lib-btcp');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
+//const io = require('socket.io');
 
 const Merchant = require('./models').Merchant;
 const Invoice = require('./models').Invoice;
@@ -13,6 +15,7 @@ const Product = require('./models').Product;
 const DUMMY_MONGO_URL = 'mongodb://localhost:27017/store-demo';
 
 // This module will be installed as a service of Bitcore, which will be running on localhost:8001.
+// TEST - `localhost:8001/store-demo/index.html`
 
 function PizzaShop(options) {
   EventEmitter.call(this);
@@ -23,34 +26,38 @@ function PizzaShop(options) {
   this.invoiceHtml = fs.readFileSync(__dirname + '/invoice.html', 'utf8');
 
   // Connect to MongoDB
-  mongoose.connect(DUMMY_MONGO_URL);
+  mongoose.connect(options.mongoURL || DUMMY_MONGO_URL, null)
+  .then(() => {
 
-  Merchant.findOne({})
-  .select('xpub')
-  .exec()
-  .then(m => {
-    if (!m || m.xpub == null) {
-      return res.status(500).send({error: "xpub hasn't been set!!! Run `node generate_hd_wallets` offline."})
-    } else {
-      this.xpub = m.xpub;
-    }
-  })
-  .catch(e => {
-    console.error(e);
-    return res.status(500).send({error: 'Failed to find Merchant in Mongo'});
-  });
+    Merchant.findOne({})
+    .select('xpub')
+    .exec()
+    .then(m => {
+      if (!m || m.xpub == null) {
+	return mongoose.Promise.reject("xpub hasn't been set!!! Run `node generate_hd_wallets` offline.");
+      } else {
+	this.xpub = m.xpub;
+      }
+    })
+    .catch(e => {
+      this.log.error(e);
+    });
+  }, e => { this.log.error(e.message); });
+
 
   //TODO Implement State Machine: AWAITING_PAYMENT -> FULL_AMOUNT_RECEIVED / TIMED_OUT / PARTIAL_AMOUNT_RECEIVED
   //TODO reuse code from invoice.html (client)
 
+/* TODO socket config
   var socket = io('http://localhost:8001');
   socket.emit('subscribe', 'bitcoind/addresstxid', ['{{address}}']);
   socket.on('bitcoind/addresstxid', function(data) {
    var address = bitcore.Address(data.address);
-   console.log(address);
+   this.log.info(address);
      //TODO save an entry in db for each confirmed payment, for each relevant addr
      // index (or address), tx_id, address_paid, amount_paid, latest_paid_time, total_satoshis
   });
+*/
 
   //TODO disconnect mongoose, socket.io
 
@@ -111,7 +118,7 @@ PizzaShop.prototype.setupRoutes = function(app, express) {
       return res.status(200).send(self.buildInvoiceHTML(i.address_index, i.total_satoshis));
     })
     .catch(e => {
-      console.error(e);
+      this.log.error(e);
       return res.status(500).send({error: 'Failed to find Merchant/create Invoice in Mongo'});
     });
   });
